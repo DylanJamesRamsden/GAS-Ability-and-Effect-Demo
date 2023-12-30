b549ffc76d401278f012d3df12992b7c150438de
Dylan James Ramsden <43576324+DylanJamesRamsden@users.noreply.github.com>
2023-12-30 12:40:23 +0200

    Created DTestActor

---
+++ Source/AbilityAndEffectDemo/DTestActor.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "GameFramework/Actor.h"
+#include "DTestActor.generated.h"
+
+UCLASS()
+class ABILITYANDEFFECTDEMO_API ADTestActor : public AActor, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ADTestActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
