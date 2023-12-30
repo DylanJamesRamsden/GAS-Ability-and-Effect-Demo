506f1c0313e6f05eac7c00994ffac429e1b42187
Dylan James Ramsden <43576324+DylanJamesRamsden@users.noreply.github.com>
2023-12-30 12:23:23 +0200

    Created DAttributeSet

---
+++ Source/AbilityAndEffectDemo/DAttributeSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "DAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ABILITYANDEFFECTDEMO_API UDAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
